fa3c7da02dd4e49f3631e9872b98d7cc839b52b4
Jordan Geltch-Robb <jgeltchrobb@hotmail.com>
2018-03-03 15:50:40 +1000

    version 0.7

--- lib/the_spot_app_v0.7.rb
+++ lib/the_spot_app_v0.7.rb
@@ -3,7 +3,7 @@ require 'geokit'
 
 Geokit::default_units = :kms
 Geokit::Geocoders::request_timeout = 3
-Geokit::Geocoders::GoogleGeocoder.api_key = 'AIzaSyDJC7s7rtUPQr18ymAqPm_CHeqxx-s8RIE'
+Geokit::Geocoders::GoogleGeocoder.api_key = 'AIzaSyAukwxtdEcmLeSsBTCTbeG-ODyz6rC5sZM' 
 
 
 beaches_coords = {
@@ -18,8 +18,8 @@ beaches_coords = {
     evans_head: {lat: -29.1086, lon: 153.4217},
     palmer_island: {lat: -29.4332, lon: 153.3406}
 }
-options = { units: "metric", APPID: "c60d3ad619a65ce0a4f912801a20afaf" }
-descriptions = ["clear sky", "few clouds", "scattered clouds", "broken clouds", "shower rain", "rain", "thunderstorm", "snow", "mist"]
+options = { units: "metric", APPID: "0d9a30cbcb91b1b90fcf974a61b6ee2f" }
+descriptions = ["clear sky", "few clouds", "scattered clouds", "broken clouds", "overcast clouds", "light rain", "shower rain", "rain", "thunderstorm", "snow", "mist"]
 
 beaches = ["Tewantin QLD", "Mooloolaba QLD", "Donnybrook QLD", "Margate QLD", "Burleigh Heads QLD", "Bilinga QLD", "Byron Bay NSW", "Ballina NSW", "Evans Head NSW", "Palmer Island NSW"]
 data_batch = []
@@ -35,13 +35,13 @@ surf_wind_max = 7
 surf_swell_min = 0.5
 kite_results = []
 kite_hash = {}
-kite_wind_min = 14
+kite_wind_min = 12
 kite_wind_max = 36
-kite_swell_max = 2.5
+kite_swell_max = 3
 beachday_hash = {}
 beachday_results = []
 beachday_temp_min = 24
-beachday_cloud_max = 80
+beachday_cloud_max = 75
 beachday_wind_max = 15
 wind_spd_kts = 1.94
 name_count = 0
@@ -50,7 +50,7 @@ activity = nil
 # Surf  method
 def surf(surf_hash, beaches, formatted_data, descriptions, surf_results, surf_swell_min, surf_wind_max)
     beaches.each do |beach|
-        if formatted_data[beach][:swell_height] > surf_swell_min && formatted_data[beach][:wind_spd_kts] < surf_wind_max && (formatted_data[beach][:description] == descriptions[0] || formatted_data[beach][:description] == descriptions[1] || formatted_data[beach][:description] == descriptions[2] || formatted_data[beach][:description] == descriptions[3] || formatted_data[beach][:description] == descriptions[4] || formatted_data[beach][:description] == descriptions[5])
+        if formatted_data[beach][:swell_height] > surf_swell_min && formatted_data[beach][:wind_spd_kts] < surf_wind_max && (formatted_data[beach][:description] == descriptions[0] || formatted_data[beach][:description] == descriptions[1] || formatted_data[beach][:description] == descriptions[2] || formatted_data[beach][:description] == descriptions[3] || formatted_data[beach][:description] == descriptions[4] || formatted_data[beach][:description] == descriptions[5] || formatted_data[beach][:description] == descriptions[6])
             surf_results.push("Y")
         else
             surf_results.push("N")
@@ -60,7 +60,7 @@ end
 # Kitesurf method
 def kite(kite_hash, beaches, formatted_data, descriptions, kite_results, kite_swell_max, kite_wind_max, kite_wind_min)
     beaches.each do |beach|
-        if formatted_data[beach][:swell_height] < kite_swell_max && formatted_data[beach][:wind_spd_kts] < kite_wind_max && formatted_data[beach][:wind_spd_kts] > kite_wind_min && (formatted_data[beach][:description] == descriptions[0] || formatted_data[beach][:description] == descriptions[1] || formatted_data[beach][:description] == descriptions[2] || formatted_data[beach][:description] == descriptions[3] || formatted_data[beach][:description] == descriptions[4])
+        if formatted_data[beach][:swell_height] < kite_swell_max && formatted_data[beach][:wind_spd_kts] < kite_wind_max && formatted_data[beach][:wind_spd_kts] > kite_wind_min && (formatted_data[beach][:description] == descriptions[0] || formatted_data[beach][:description] == descriptions[1] || formatted_data[beach][:description] == descriptions[2] || formatted_data[beach][:description] == descriptions[3] || formatted_data[beach][:description] == descriptions[4] || formatted_data[beach][:description] == descriptions[5] || formatted_data[beach][:description] == descriptions[6])
             kite_results.push("Y")
         else
             kite_results.push("N")
@@ -82,10 +82,11 @@ beaches_coords.each do |key, value|
     data_batch.push(OpenWeather::Current.geocode(value[:lat], value[:lon], options))
 end
 # Format and push relevant data from data_batch into formatted_data
+# Add swell height and convert wind to knots
 data_batch.each do |hash|
     swell_height = rand(1..6)/2.0
     hash["name"] = beaches[name_count]
-    formatted_data[hash["name"]] = {"description": hash["weather"][0]["description"], "wind_spd_kts": hash["wind"]["speed"]* 1.94, "cloud_coverage": hash["clouds"]["all"], "temp_max": hash["main"]["temp_max"], "swell_height": swell_height}
+    formatted_data[hash["name"]] = {"description": hash["weather"][0]["description"], "wind_spd_kts": hash["wind"]["speed"]* wind_spd_kts, "cloud_coverage": hash["clouds"]["all"], "temp_max": hash["main"]["temp_max"], "swell_height": swell_height}
     name_count += 1
 end
 # Parse beach names through geokit to generate beach_locations
@@ -108,9 +109,21 @@ loop do
         break
     end
 end
+user_loc = ""
+user_coords = ""
 # Parse users location through geokit to generate distances array
 puts "Where are you now? e.g. brisbane QLD"
-user_loc = Geokit::Geocoders::GoogleGeocoder.geocode(gets.chomp.capitalize)
+loop do
+  user_loc = Geokit::Geocoders::GoogleGeocoder.geocode(gets.chomp.capitalize)
+  user_coords = user_loc.ll
+  if user_coords =~ /\d/         # Calling String's =~ method.
+    # puts "The String #{user_coords} has a number in it."
+    break
+  else
+    # puts "The String #{user_coords} does not have a number in it."
+    puts "Please enter a valid location"
+  end
+end
 beach_locations.each do |beach_loc|
     distances.push(user_loc.distance_to(beach_loc))
 end
@@ -141,15 +154,20 @@ if activity == "Surf"
         puts ""
         puts "The closest suitable surfing spot is #{key} - #{value.to_i} kms"
         puts ""
-        puts "... and the other suitable options are:"
-        puts ""
+        if spots_distances.length != 1
+          puts "... and the other suitable options are:"
+          puts ""
+        end
       else
         others.push("#{key} - #{value.to_i} kms")
       end
+      # if others.length == 0
+      # end
     end
     # If there are no suitable locations - Outpout
     if spots.length == 0
         puts "Sorry, there are currently no locations suitable for this activity"
+        puts "Try again later"
     end
     puts others
     puts ""
@@ -190,6 +208,7 @@ elsif activity == "Kite"
     # If there are no suitable locations - Outpout
     if spots.length == 0
     puts "Sorry, there are currently no locations suitable for this activity"
+    puts "Try again later"
     end
   puts others
   puts ""
@@ -230,6 +249,7 @@ elsif activity == "Beachday"
   # If there are no suitable locations - Outpout
   if spots.length == 0
     puts "Sorry, there are currently no locations suitable for this activity"
+    puts "Try again later"
     end
   puts others
   puts ""
